a153985d18a49e8cae46f16063563ca17bb9f418
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-12 13:54:20 +0300

    Add scripts for clickable behavior

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#clickable1").click(function() {
+    $("#showing1").fadeToggle();
+    $("#hidden1").fadeToggle();
+  });
+
+  $("#clickable2").click(function() {
+    $("#showing2").fadeToggle();
+    $("#hidden2").fadeToggle();
+  });
+
+  $("#clickable3").click(function() {
+    $("#showing3").fadeToggle();
+    $("#hidden3").fadeToggle();
+  });
+
+  $("#clickable4").click(function() {
+    $("#showing4").fadeToggle();
+    $("#hidden4").fadeToggle();
+  });
+
+  $("#clickable5").click(function() {
+    $("#showing5").fadeToggle();
+    $("#hidden5").fadeToggle();
+  });
+
+});
